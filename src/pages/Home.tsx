2d630be18e27576f90a23aa3355b12a6fab50b47
Rafael <rafaelmerlotto@gmail.com>
2025-09-11 20:56:43 +0200

    Change chat background color

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Main from '../camponents/Main'
 export default function Home() {
     return (
         <React.Fragment>
-            <div className="flex flex-col h-screen bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col h-screen bg-cyan-900">
                 <Main />
             </div>
         </React.Fragment>
